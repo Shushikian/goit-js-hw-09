d4a30b2b932a505890e17ae8a20f717de11ca9a0
Dmitry Shushaiev <shushikian@gmail.com>
2022-07-02 00:48:25 +0300

    hw 1 - done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const btnStart = document.querySelector(`[data-start]`);
+const btnStop = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
 
+let timerId = null;
+btnStop.disabled = true;
+btnStart.disabled = false;
+
+function start() {
+    btnStart.disabled = true;
+    btnStop.disabled = false;
+    timerId = setInterval(() => { 
+        body.style.backgroundColor = getRandomHexColor();
+    }, 1000)
+}
+
+function stop() {
+    clearInterval(timerId);
+    btnStart.disabled = false;
+    btnStop.disabled = true;
+}
+
+btnStart.addEventListener("click", start);
+btnStop.addEventListener("click", stop);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
